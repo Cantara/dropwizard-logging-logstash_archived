5a50496077f35006cf3a271c74f2bdc48fd21f68
baardl <bard.lind@gmail.com>
2016-10-04 11:58:51 +0200

    #1 Support for Dropwizard 1.0.2

--- src/main/java/no/cantara/dropwizard/logging/LogstashFileAppenderFactory.java
+++ src/main/java/no/cantara/dropwizard/logging/LogstashFileAppenderFactory.java
@@ -1,13 +1,17 @@
 package no.cantara.dropwizard.logging;
 
+import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.filter.ThresholdFilter;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.Appender;
 import ch.qos.logback.core.FileAppender;
 import ch.qos.logback.core.Layout;
+import ch.qos.logback.core.filter.Filter;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 import io.dropwizard.logging.FileAppenderFactory;
+import io.dropwizard.logging.async.AsyncAppenderFactory;
 import net.logstash.logback.encoder.LogstashEncoder;
 
 @JsonTypeName("logstashfile")
@@ -25,8 +29,11 @@ public class LogstashFileAppenderFactory extends FileAppenderFactory {
         this.customFields = customFields;
     }
 
-    @Override
     public Appender<ILoggingEvent> build(LoggerContext context, String applicationName, Layout<ILoggingEvent> layout) {
+        return this.build(context,applicationName, layout,null);
+    }
+
+    public Appender<ILoggingEvent> build(LoggerContext context, String applicationName, Layout<ILoggingEvent> layout,AsyncAppenderFactory asyncAppenderFactory) {
         final FileAppender<ILoggingEvent> appender = buildAppender(context);
         appender.setName("logstashfile-appender");
 
@@ -41,10 +48,19 @@ public class LogstashFileAppenderFactory extends FileAppenderFactory {
         appender.setEncoder(logstashEncoder);
 
         appender.setPrudent(false);
-        addThresholdFilter(appender, threshold);
+//        addThresholdFilter(appender, threshold);
+        Filter<ILoggingEvent> thresholdFilter = createThresholdFilter(threshold);
+        appender.addFilter(thresholdFilter);
         appender.stop();
         appender.start();
 
-        return wrapAsync(appender);
+        return wrapAsync(appender,asyncAppenderFactory);
+    }
+
+    private Filter<ILoggingEvent> createThresholdFilter(Level threshold) {
+        final ThresholdFilter filter = new ThresholdFilter();
+        filter.setLevel(threshold.toString());
+        filter.start();
+        return filter;
     }
 }
